c77d18a75233c389266b22c8d111ac4ea6ba7e74
Z <z@debian.home.net>
2018-03-20 20:29:29 -0500

    prep for production

--- homesite/urls.py
+++ homesite/urls.py
@@ -1,4 +1,4 @@
-"""djangoSite URL Configuration
+"""homesite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -24,7 +24,6 @@ urlpatterns = [
 ]
 
 urlpatterns += [
-    path('blog/', include('blog.urls')),
     path('', include('blog.urls'))
 
 ]
